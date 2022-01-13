85a74db2f87b4b3ec62c0a8c7ab442e8aa1b5a17
FrenchClavier <59101943+FrenchClavier@users.noreply.github.com>
2022-01-13 09:09:12 +0100

    Add files via upload

---
+++ app/Models/confirmation-connexion.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include "fonction-page-accueil.php";
+
+include "config-page-accueil.php";
+$_SESSION['connecté'] = FALSE;
+# Première méthode 
+$var_recup = GETPDO($config);
+$var_recup_ex = $var_recup->prepare("SELECT `identifiant`, `motDePasse` FROM authentification");
+$var_recup_ex->execute();
+$données_verif = $var_recup_ex->fetchAll();
+
+# Deuxième méthode 
+$X = $_POST['identifiant-co'];
+$Y = $_POST['mdp-co']; 
+$recup_user = $var_recup->prepare('SELECT * FROM authentification WHERE identifiant = ? AND motDePasse = ? ');
+$recup_user->execute(array($X, $Y)); 
+
+# Assimilation de données aux variables de sessions
+$_SESSION['idd'] = $X;
+
+
+
+if (!empty($_POST['identifiant-co'] && $_POST['mdp-co'])) {
+    foreach ($données_verif as $données_verif) {
+        echo "zebi <br>";
+        echo($données_verif['identifiant'] . " ");
+        echo($données_verif['motDePasse'] . " ");
+
+        /* if ($données_verif['identifiant'] === $X &&
+       $données_verif['motDePasse'] === $Y) {
+           header('location:index.php'); 
+       } */
+       if ($recup_user->rowCount() > 0) {
+           header('location: http://localhost:3000/Frais/FicheFrais2.php');
+           $_SESSION['connecté'] = TRUE;
+       } 
+       else {
+           #header('location:page-inscription.php');
+           echo "<script type=\"text/javascript\">window.alert ('Identifiant et/ou Mot de passe incorrect'); 
+        window.location='index.php'; </script>";
+           $_SESSION['connecté'] = FALSE;
+       } 
+    }
+}
+else {
+    echo "<script type=\"text/javascript\">window.alert ('Veuillez remplir les champs de connexion'); 
+        window.location='index.php'; </script>";
+}
+
+?>
---
+++ app/Views/inscription.php
@@ -0,0 +1,241 @@
+<?php
+ session_start();
+ if (isset($_SESSION['connecté'])) {
+    $_SESSION['connecté'] = FALSE;
+}
+ /*
+$_SESSION['nom'] = "Bonal";
+$_SESSION['prenom'] = "Côme"; */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>         <!-- Définition du style css !-->
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
+
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;	
+	font-family: Raleway, sans-serif;
+}
+
+body {
+	background: linear-gradient(90deg, #C7C5F4, #776BCC);		
+}
+
+label {
+
+    padding-left: 10px;
+    margin-left: 10px;
+}
+
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 100vh;
+ 
+}
+
+.screen {		
+	background: linear-gradient(90deg, #5D54A4, #7C78B8);		
+	position: relative;	
+	height: 600px;
+	width: 360px;	
+	box-shadow: 0px 0px 24px #5C5696;
+    border-radius: 30px;
+}
+
+.screen__content {
+	z-index: 1;
+	position: relative;	
+	height: 100%;
+    padding-left: 10px;
+    margin-top: 20px;
+}
+
+.screen__background {		
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	z-index: 0;
+	-webkit-clip-path: inset(0 0 0 0);
+	clip-path: inset(0 0 0 0);	
+}
+
+.screen__background__shape {
+	transform: rotate(45deg);
+	position: absolute;
+}
+
+.screen__background__shape1 {
+	height: 520px;
+	width: 520px;
+	background: #FFF;	
+	top: -50px;
+	right: 120px;	
+	border-radius: 0 72px 0 0;
+}
+
+.screen__background__shape2 {
+	height: 220px;
+	width: 220px;
+	background: #6C63AC;	
+	top: -172px;
+	right: 0;	
+	border-radius: 32px;
+}
+
+.screen__background__shape3 {
+	height: 540px;
+	width: 190px;
+	background: linear-gradient(270deg, #5D54A4, #6A679E);
+	top: -24px;
+	right: 0;	
+	border-radius: 32px;
+}
+
+.screen__background__shape4 {
+	height: 400px;
+	width: 200px;
+	background: #7E7BB9;	
+	top: 420px;
+	right: 50px;	
+	border-radius: 60px;
+}
+
+.login {
+	width: 320px;
+	padding: 30px;
+	padding-top: 156px;
+}
+
+.login__field {
+	padding: 20px 10px;	
+	position: relative;
+
+}
+.login__input {
+	border: none;
+	border-bottom: 2px solid #D1D1D4;
+	background: none;
+
+	padding-left: 24px;
+	font-weight: 700;
+	width: 75%;
+	transition: .2s;
+    margin-top: 13px;	
+    margin-bottom: 13px;
+
+}
+
+.login__input:active,
+.login__input:focus,
+.login__input:hover {
+	outline: none;
+	border-bottom-color: #6A679E;
+}
+
+.login__submit {
+	background: #fff;
+	font-size: 14px;
+	margin-top: 20px;
+	padding: 16px 20px;
+	border-radius: 26px;
+	border: 1px solid #D4D3E8;
+	text-transform: uppercase;
+	font-weight: 700;
+	display: flex;
+	align-items: center;
+	width: 95%;
+	color: #4C489D;
+	box-shadow: 0px 2px 2px #5C5696;
+	cursor: pointer;
+	transition: .2s;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+}
+
+.login__submit:active,
+.login__submit:focus,
+.login__submit:hover {
+	border-color: #6A679E;
+	outline: none;
+}
+        /*fieldset {
+           width: 70%;
+           margin: 0 auto;
+           border: 5px dotted gray;
+           margin-bottom: 20px;
+           background-color: #FAEBD7;
+       } */
+       legend {
+           color: purple;
+           font-size: 1.2em;
+           padding: 10px 10px;
+           text-align: left;
+       }
+       input {
+           padding: 5px;
+           margin: 5px;
+       }
+       
+       h1 {
+           font-size: 1.2em;
+       }
+       
+       p {
+       
+       text-align: left;
+   }
+        </style> 
+ 
+
+<div class="container">
+	<div class="screen">
+		        <div class="screen__content">
+                        
+                    <!-- Formulaire de connexion -->
+                    <form action="http://localhost:3000/Inscription&connexion/page-inscription.php" method="POST">
+                        <label>Nom : </label> <br>
+                        <input type="text" name="nom"class="login__input"/>
+                      
+                        <label>Prénom : </label>
+                        <input type="text" name="prenom" class="login__input"/>
+
+                        <label>Adresse mail : </label>
+                        <input type="text" name="mail" class="login__input"/>
+
+                        <label>Identifiant : </label> <br>
+                        <input type="text" name="identifiant" class="login__input"/> <br>
+
+                        <label>Mot de passe : </label> <br>
+                        <input type="password" name="mdp" class="login__input"/>          
+
+				        <input class="login__submit" type="submit" value="S'inscrire">
+                        </input>
+                    </form>
+                    <!-- Formulaire d'inscription -->
+                    <form action="http://localhost:3000/Inscription&connexion/index.php" method="POST">                  
+                        <input class="login__submit" type="submit" value="Connexion">	
+                    </form>		
+            </div>
+        <!-- Définition des arrières plan superposés-->
+		<div class="screen__background"> 
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	
+</body>
+</html>
