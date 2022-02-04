2ed1e11a25d7edf1cb2cdee7ceaf2bdb877cab08
Comies34 <come.bonal@gmail.com>
2022-02-04 14:25:36 +0100

    no message

--- app/Views/noteDeFrais.php
+++ app/Views/noteDeFrais.php
@@ -6,13 +6,16 @@
     <title>Document</title>
 </head>
 <body>
+<?php include "menu.php"; ?> 
 
 <style>
 
 .Liam{
     font-family: Arial, Helvetica, sans-serif;
     border-collapse: collapse;
-    width: 100%;
+    width: 70%;
+    text-align: center ;
+    margin: auto;
   }
   
   .Liam td, .Liam th {
@@ -28,37 +31,40 @@
     padding-top: 12px;
     padding-bottom: 12px;
     text-align: left;
-    background-color: #2d87d1;
+    background-color: #5D54A4;
     color: white;
   }
 
 </style>
                         <table class="Liam">
                             <tr>
-                                <th>nbr_km</th>
-                                <th>cout_km</th>
-                                <th>restauration</th>
-                                <th>hotel</th>
-                                <th>evenementiel</th>
+                            <th>Numéro Factures</th>
+                                <th>Nombre de Kilomètres</th>
+                                <th>Indémnité kilométrique</th>
+                                <th>Restauration</th>
+                                <th>Hôtel</th>
+                                <th>Evènementiel</th>
                             </tr>
     <?php        
 
-                
+                $compteur = 1;
                     foreach ($dataToDisplay as $fetch20) {
                         ?>
                             <tr>
+                              <td> <?php echo $compteur; ?> </td>
                                 <td><?php echo $fetch20['nbr_km']; ?></td>
                                 <td><?php echo $fetch20['cout_km']; ?></td>
                                 <td><?php echo $fetch20['restauration']; ?></td>
                                 <td><?php echo $fetch20['hotel']; ?></td>
                                 <td><?php echo $fetch20['evenementiel']; ?></td>
                             </tr>
-                        <?php
+                            
+                        <?php $compteur = $compteur + 1;
                         }
                 ?>
                 </table>
 
           
                     
-    </body>
+</body>
 </html>
